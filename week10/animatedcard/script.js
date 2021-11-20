c5f5797ff8441d7ef6d1b1ab95ed58c389c80aaf
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-11-20 16:34:02 -0500

    keyframe animations

---
+++ week10/animatedcard/script.js
@@ -0,0 +1,64 @@
+const startBtn = document.querySelectorAll(".red");
+startBtn.addEventListener('click', startAnimation(), false);
+
+function startAnimation() {
+  var red = document.querySelector("red");
+  console.log(red);
+  // if (red.classList != "red-animate") {
+  //   red.classList.add("red-animate");
+  // }
+}
+
+
+
+
+
+function useThisStyle {
+  .purple {
+    background - color: $purple;
+  }
+
+  .purple - animate {
+      animation: swatch - purple - motion 5 s cubic - bezier(.4, .0, .2, 1) infinite;
+    }
+
+    .blue {
+      background - color: $blue;
+    }
+
+    .blue - animate {
+      animation: swatch - blue - motion 5 s cubic - bezier(.4, .0, .2, 1) infinite;
+    }
+
+    .green {
+      background - color: $green;
+    }
+
+    .green - animate {
+      animation: swatch - green - motion 5 s cubic - bezier(.4, .0, .2, 1) infinite;
+    }
+
+    .yellow {
+      background - color: $yellow;
+    }
+
+    .yellow - animate {
+      animation: swatch - yellow - motion 5 s cubic - bezier(.4, .0, .2, 1) infinite;
+    }
+
+    .orange {
+      background - color: $orange;
+    }
+
+    .orange - animate {
+      animation: swatch - orange - motion 5 s cubic - bezier(.4, .0, .2, 1) infinite;
+    }
+
+    .red {
+      background - color: $red;
+    }
+
+    .red - animate {
+      animation: swatch - red - motion 5 s cubic - bezier(.4, .0, .2, 1) infinite;
+    }
+}
